a4b049890008b20c778c3e57f24fc21d69b990c5
Thiago Alves <thiago.765.ta@gmail.com>
2023-09-26 20:01:53 -0300

    eventos DOM adicionados ao botão

--- index.js
+++ index.js
@@ -8,5 +8,24 @@ function calc(){
     window.document.getElementById("resultado").innerHTML = `Energia = ${energiaFormatada} joules`;
 };
 
+var button = window.document.getElementById("b1")
+button.addEventListener("mouseenter", enter)
+button.addEventListener("mouseleave", out)
+button.addEventListener("mouseup", click_out)
+button.addEventListener("mousedown", click_on)
 
+function enter() {
+    this.style.backgroundColor = "orangered" 
+}
 
+function out() {
+    this.style.backgroundColor = "orange"
+}
+
+function click_out() {
+    this.style.backgroundColor = "orangered"
+} 
+
+function click_on() {
+    this.style.backgroundColor = "white"
+}
